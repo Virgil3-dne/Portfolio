eaeb0397ef7504e950a5d4f00b9beba1e1a8adff
Virgil3-dne <104080736+Virgil3-dne@users.noreply.github.com>
2022-05-30 09:16:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var scene3d = document.getElementById("scene3d");
-var CANVAS_WIDTH = 800;
-var CANVAS_HEIGHT = 800;
+var CANVAS_WIDTH = 500;
+var CANVAS_HEIGHT = 500;
 
 // SCENE
 
@@ -9,9 +9,9 @@ scene = new THREE.Scene();
 // CAMERA 
 
 camera = new THREE.PerspectiveCamera(45, CANVAS_WIDTH / CANVAS_HEIGHT, 0.1, 100);
-camera.position.x = 17;
-camera.position.y = 12;
-camera.position.z = 13;
+camera.position.x = -3;
+camera.position.y = 0;
+camera.position.z = 0;
 camera.lookAt(scene.position);
 
 // RENDERER
@@ -22,17 +22,17 @@ renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
 
 // GEOMETRY & MATERIALS
 
-var geometry = new THREE.BoxGeometry( 5, 5, 5 );
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 
 
 var cubeMaterials =
 [
-    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/1.png' ), side: THREE.DoubleSide } ),
-    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/2.png' ), side: THREE.DoubleSide } ),
-    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/3.png' ), side: THREE.DoubleSide } ),
-    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/4.png' ), side: THREE.DoubleSide } ),
-    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/5.png' ), side: THREE.DoubleSide } ),
-    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/6.png' ), side: THREE.DoubleSide } ),
+    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/1.png' ), shininess  : 200, side: THREE.DoubleSide } ),
+    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/2.png' ), shininess  : 200, side: THREE.DoubleSide } ),
+    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/3.png' ), shininess  : 200, side: THREE.DoubleSide } ),
+    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/4.png' ), shininess  : 200, side: THREE.DoubleSide } ),
+    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/5.png' ), shininess  : 200, side: THREE.DoubleSide } ),
+    new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load( 'img/6.png' ), shininess  : 200, side: THREE.DoubleSide } ),
 ];
 
 var material = new THREE.MeshFaceMaterial( cubeMaterials ); 
@@ -43,16 +43,18 @@ scene.add( cube );
 var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.2 );
 scene.add( ambientLight );
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.6 );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+directionalLight.position.set(-5, 0, 0);
 scene.add( directionalLight );
+
 // FINISH SCENE SETUP
 
 
 var animate = function () {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+
+	cube.rotation.y += -0.008;
 
 	renderer.render( scene, camera );
 };
